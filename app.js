ed646c90573aa4410c43006d4aac2f568e8968aa
Jaroslav Kotik <kotik.jaroslav@gmail.com>
2022-01-24 21:48:36 +0100

    release

---
+++ app.js
@@ -0,0 +1,21 @@
+const axios = require("axios")
+const express = require("express")
+
+const CONFIG = {
+  port: 3000
+}
+
+const app = express()
+
+app.get('/', (req, res) => {
+  var url = req.query.q
+  axios.get(url).then( response => {
+      res.json({
+        'url': response.request.res.responseUrl
+      })
+  }).catch( error => {
+    res.json(`something went wrong`)
+  })
+})
+
+app.listen(process.env.PORT || CONFIG.port)
