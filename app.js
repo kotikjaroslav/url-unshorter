0d3f3107c41d6f2fd4c0bc84aba11a4d40c1b476
Jaroslav Kotik <kotik.jaroslav@gmail.com>
2022-01-30 10:51:06 +0100

    added user-agent configuration

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const axios = require("axios")
 const express = require("express")
 
 const CONFIG = {
-  port: 3000
+  port: 3000,
+  user_agent: "Mozilla/5.0 (X11; Linux x86_64)"
 }
 
 const app = express()
 
 app.get('/', (req, res) => {
   var url = req.query.q
-  axios.get(url).then( response => {
+  axios.get(url, {headers:{'User-Agent':CONFIG.user_agent}}).then( response => {
       res.json({
         'url': response.request.res.responseUrl
       })
